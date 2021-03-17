85f64fc100d9cebb6d8ddebf1db4ae7c954c0aa8
Felipe Campos <fepas.unb@gmail.com>
2021-03-17 01:31:52 -0300

    adding basic flask setup
    Signed-off-by: Felipe Campos <fepas.unb@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-Para rodar o container de desenvolvimento, execute o seguinte comando:
+# gateway
+
+0- ligar o docker da sua máquina caso não esteja ligado. Caso esteja no windows
+
+        sudo service docker start
+
+1- Criar um novo arquivo com o nome ".env"
+
+2- Copiar o conteudo de .env.dev (que já está no projeto) para esse novo .env criado
+
+3- Rodar o comando:
 
         make start-dev
+
+Esse comando sempre deve ser executado quando quiser iniciar o container da gateway.
--- src/news/news.py
+++ src/news/news.py
@@ -1,7 +1,22 @@
+import os
+import requests
 from flask import Blueprint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ETL_NEWS_HOST = os.getenv('ETL_NEWS_HOST')
+ETL_NEWS_PORT = os.getenv('ETL_NEWS_PORT')
+
+base_url = f"{ETL_NEWS_HOST}:{ETL_NEWS_PORT}"
 
 news = Blueprint('news', __name__, url_prefix='/news')
 
 @news.route('/')
 def index():
     return "news"
+
+@news.route('/deputies')
+def deputies():
+    r = requests.get(f'http://{base_url}/api/deputies')
+    return r.text

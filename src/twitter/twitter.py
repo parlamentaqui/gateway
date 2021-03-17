9c8e8fdc39439654812f08781dffbd489117397c
Felipe Campos <fepas.unb@gmail.com>
2021-03-17 01:57:26 -0300

    adding twitter_etl configs
    Signed-off-by: Felipe Campos <fepas.unb@gmail.com>

--- src/twitter/twitter.py
+++ src/twitter/twitter.py
@@ -1,7 +1,23 @@
+import os
+import requests
 from flask import Blueprint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ETL_TWITTER_HOST = os.getenv('ETL_TWITTER_HOST')
+ETL_TWITTER_PORT = os.getenv('ETL_TWITTER_PORT')
+
+base_url = f"{ETL_TWITTER_HOST}:{ETL_TWITTER_PORT}"
 
 twitter = Blueprint('twitter', __name__, url_prefix='/twitter')
 
 @twitter.route('/')
 def index():
     return "twitter"
+
+@twitter.route('/tweets')
+def tweets():
+    print(f'http://{base_url}/api/tweets')
+    r = requests.get(f'http://{base_url}/api/tweets')
+    return r.text

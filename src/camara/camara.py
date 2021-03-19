a2222efca9b03211518f49a8eafab85baf996f3e
jplyra <jplyra97@gmail.com>
2021-03-18 23:26:25 -0300

    Configurações de gateway do etl_camara
    Signed-off-by: jplyra <jplyra97@gmail.com>

--- src/camara/camara.py
+++ src/camara/camara.py
@@ -1,7 +1,22 @@
+import os
+import requests
 from flask import Blueprint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ETL_CAMARA_HOST = os.getenv('ETL_CAMARA_HOST')
+ETL_CAMARA_PORT = os.getenv('ETL_CAMARA_PORT')
+
+base_url = f"{ETL_CAMARA_HOST}:{ETL_CAMARA_PORT}"
 
 camara = Blueprint('camara', __name__, url_prefix='/camara')
 
 @camara.route('/')
 def index():
-    return "camara!"
+    return "Camara"
+
+@camara.route('/deputies')
+def deputies():
+    r = requests.get(f'http://{base_url}/api/deputies')
+    return r.json()

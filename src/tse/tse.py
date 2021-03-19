61774ffeabd35ef52d7e0277caeb52fe249e0e15
jplyra <jplyra97@gmail.com>
2021-03-19 19:26:04 -0300

    Setup base da conexão entre o gateway e o etl_tse, agora o gateway consegue se comunicar corretamente com o docker do tse.
    Signed-off-by: jplyra <jplyra97@gmail.com>

--- src/tse/tse.py
+++ src/tse/tse.py
@@ -1,7 +1,22 @@
+import os
+import requests
 from flask import Blueprint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ETL_TSE_HOST = os.getenv('ETL_TSE_HOST')
+ETL_TSE_PORT = os.getenv('ETL_TSE_PORT')
+
+base_url = f"{ETL_TSE_HOST}:{ETL_TSE_PORT}"
 
 tse = Blueprint('tse', __name__, url_prefix='/tse')
 
 @tse.route('/')
 def index():
     return "tse"
+
+@tse.route('/deputies')
+def deputies():
+    r = requests.get(f'http://{base_url}/api/deputies')
+    return r.json()

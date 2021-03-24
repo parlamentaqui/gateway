cb529407179d198f17c863c6b9b9b8ce272bbe7c
Felipe Campos <fepas.unb@gmail.com>
2021-03-23 22:25:45 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,6 +3,9 @@ from news.news import news
 from twitter.twitter import twitter
 from camara.camara import camara
 from tse.tse import tse
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(news)
@@ -10,9 +13,11 @@ app.register_blueprint(twitter)
 app.register_blueprint(camara)
 app.register_blueprint(tse)
 
+PORT = os.getenv('PORT')
+
 @app.route('/')
 def index():
     return "Parlamentaqui Gateway!!!"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=PORT)

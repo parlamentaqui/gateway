8d402759d6ec7c817e8957263a9b4e5cf8dc7a36
jplyra <jplyra97@gmail.com>
2021-05-14 12:59:29 -0300

    alterando a rota do /deputies e a /home pelas respectivas rotas corretas no etl_camara
    Signed-off-by: jplyra <jplyra97@gmail.com>

--- src/camara/camara.py
+++ src/camara/camara.py
@@ -18,12 +18,12 @@ def index():
 
 @camara.route('/deputies')
 def deputies():
-    r = requests.get(f'http://{base_url}/api/deputies-home')
+    r = requests.get(f'http://{base_url}/api/deputies')
     return jsonify(r.json())
 
 @camara.route('/home')
 def deputies_home():
-    r = requests.get(f'http://{base_url}/api/deputies')
+    r = requests.get(f'http://{base_url}/api/deputies-home')
     return jsonify(r.json())
 
 @camara.route('/resultado', methods=['POST'])

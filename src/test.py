1c60d4f3d36598f5c0f75e5f2b0600780def4e20
lmmLucasMachado <lmlucasmachadom@gmail.com>
2021-05-18 19:02:15 -0300

    Testes News

--- src/test.py
+++ src/test.py
@@ -41,47 +41,76 @@ class CamaraTests(unittest.TestCase):
         self.assertEqual(200 , request.status_code)
         self.assertEqual('Camara' , request.data.decode())
     
-    def test_index_deputies_status(self):
+    def test_deputies_status(self):
         request = self.client.get('/camara/deputies')
         self.assertEqual(200 , request.status_code)
 
-    def test_index_home_status(self):
+    def test_home_status(self):
         request = self.client.get('/camara/home')
         self.assertEqual(200 , request.status_code)
 
-    def test_index_profile_id_status(self):
+    def test_profile_id_status(self):
         request = self.client.get('/camara/profile/3')
         self.assertEqual(200 , request.status_code)
 
 
-    def test_index_parties_status(self):
+    def test_parties_status(self):
         request = self.client.get('/camara/parties')
         self.assertEqual(200 , request.status_code)
 
 
-    def test_index_federative_unities_status(self):
+    def test_federative_unities_status(self):
         request = self.client.get('/camara/federative_unities')
         self.assertEqual(200 , request.status_code)
 
-    def test_index_expenses_status(self):
+    def test_expenses_status(self):
         request = self.client.get('/camara/expenses')
         self.assertEqual(200 , request.status_code)
 
-    def test_index_expenses_id_status(self):
+    def test_expenses_id_status(self):
         request = self.client.get('/camara/expenses/3')
         self.assertEqual(200 , request.status_code)
 
-    def test_index_get_votes_by_deputy_id_status(self):
+    def test_get_votes_by_deputy_id_status(self):
         request = self.client.get('/camara/get_votes_by_deputy_id/3')
         self.assertEqual(200 , request.status_code)
 
-    def test_index_get_proposition_by_id_status(self):
+    def test_get_proposition_by_id_status(self):
         request = self.client.get('/camara/get_proposition_by_id')
         self.assertEqual(200 , request.status_code)
 
     def tearDown(self):
         self.context.pop()
 
+
+class NewsTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app
+        self.app.testing = True
+        self.context = self.app.test_request_context()
+        self.context.push()
+        self.client = self.app.test_client()
+
+    def test_index_camara_status(self):
+        request = self.client.get('/news')
+        self.assertEqual(200 , request.status_code)
+        self.assertEqual('news' , request.data.decode())
+
+    def test_deputies_status(self):
+        request = self.client.get('/news/deputies')
+        self.assertEqual(200 , request.status_code)
+
+    def test_latestNews_status(self):
+        request = self.client.get('/news/latestNews')
+        self.assertEqual(200 , request.status_code)
+
+    def test_latestNews_id_status(self):
+        request = self.client.get('/news/latestNews/3')
+        self.assertEqual(200 , request.status_code)
+
+    def tearDown(self):
+        self.context.pop()
 
 if __name__=='__main__':
     unittest.main()

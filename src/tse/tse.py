b50943aa016209e63bb35e32329b1bf01ccd2337
jplyra <jplyra97@gmail.com>
2021-05-19 19:03:10 -0300

    Fix #103 rotas de redirecionamento da api criadas.
    Signed-off-by: jplyra <jplyra97@gmail.com>

--- src/tse/tse.py
+++ src/tse/tse.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,3 +20,23 @@ def index():
 def deputies():
     r = requests.get(f'http://{base_url}/api/deputies')
     return r.json()
+
+@tse.route('/get_deputies_equity/<id>')
+def get_deputies_equity(id):
+    r = requests.get(f'http://{base_url}/api/get_deputies_equity/{id}')
+    return jsonify(r.json())
+
+@tse.route('/get_filtered_deputies_equity', methods=['POST'])
+def get_filtered_deputies_equity():
+    r = requests.post(f'http://{base_url}/api/get_filtered_deputies_equity', json=request.get_json())
+    return jsonify(r.json())
+
+@tse.route('/get_total_value_deputies_equity/<id>')
+def get_total_value_deputies_equity(id):
+    r = requests.get(f'http://{base_url}/api/get_total_value_deputies_equity/{id}')
+    return jsonify(r.json())
+    
+@tse.route('/csv_deputies')
+def csv_deputies():
+    r = requests.get(f'http://{base_url}/api/csv_deputies')
+    return jsonify(r.json())
